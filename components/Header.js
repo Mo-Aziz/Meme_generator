3d2e45522f332447ea79312217fe6f0a289e51c0
Mo Aziz <Mohamed.aref32@gmail.com>
2020-05-23 03:10:29 -0500

    Initial commit

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "../src/styles.css";
+
+
+
+function Header(){
+return(
+  <div>
+<header>
+<img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" alt="troll face"/>
+<p> MEME GENERATOR</p>
+</header>
+
+</div>
+)
+} 
+export default Header
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Header from "../components/Header"
+import "./styles.css";
+import MemeGenerator from "../components/MemeGenerator";
+
+class App extends Component{
+render(){
+  return(
+    <div>
+<Header />
+<MemeGenerator />
+
+    </div>
+  )
+}
+}
+
+export default App
